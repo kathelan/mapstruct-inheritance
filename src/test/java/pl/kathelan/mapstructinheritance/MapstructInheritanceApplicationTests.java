5a64e82c31638f77992b0e7e17b05ca5d0163fd2
kathelan <mateuszdeptulski@gmail.com>
2024-01-24 16:14:01 +0100

    Initial commit

---
+++ src/test/java/pl/kathelan/mapstructinheritance/MapstructInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kathelan.mapstructinheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructInheritanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
